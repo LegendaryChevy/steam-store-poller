5270b14ab2413ba8d0bdf2340640e2b13ec50a90
legendarychevy <leeson73@gmail.com>
2024-05-02 00:27:43 -0700

    working model

--- game-filter.py
+++ game-filter.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import time
+import os
 import mysql.connector
 import re
 from steam_store_poller import poll_steam
@@ -13,19 +14,49 @@ from games_list_comparer import comparer
    # database=""
    # )
 
-#poll_steam()
 
-#comparer()
+def check_and_create_files(file_names: list):
+    basic_structure = {"applist": {"apps": []}}
+
+    for name in file_names:
+        # Check if file exists
+        if os.path.isfile(name):
+            with open(name, 'r') as f:
+                # Load existing data
+                try:
+                    data = json.load(f)
+                except json.JSONDecodeError:
+                    # If file does not contain valid JSON, overwrite with basic structure
+                    data = basic_structure
+
+                # Check if data has basic structure
+                if "applist" not in data or "apps" not in data.get("applist", {}):
+                    # If basic structure is not present, add it
+                    data["applist"] = {"apps": []}
+
+            # Write updated data to file
+            with open(name, 'w') as f:
+                json.dump(data, f)
+        else:
+            # If the file does not exist, create it with the basic structure
+            with open(name, 'w') as f:
+                json.dump(basic_structure, f)
+
+check_and_create_files(['new_games.json', 'old_games.json'])
+
+poll_steam()
+
+comparer()
 
 #mycursor = mydb.cursor()
 
 with open('new_games.json') as json_file:
     data = json.load(json_file)
-    all_games_data = []
+    all_games_data = {}
     counter = 0
     for game in data['applist']['apps']:
         vr_tag_present = False
-        if counter >= 20:
+        if counter >= 5:
             break
         
         appid = game['appid']
@@ -34,27 +65,28 @@ with open('new_games.json') as json_file:
         #print(f"Getting details for game {game_name} with appid {appid}")
         
         response = requests.get(f'http://store.steampowered.com/api/appdetails/?appids={appid}')
-
+        
+        #print(response.status_code)
         if response.status_code == 200:
 
             game_data = response.json()
-           
-            
-            if str(appid) in game_data and 'data' in game_data[str(appid)]:
-                 game_details = game_data[str(appid)]['data']  # Accessing the game details
 
+            if str(appid) in game_data and 'data' in game_data[str(appid)]:
+                game_details = game_data[str(appid)]['data']  # Accessing the game details
+            
            # if game_details is not None:
             #    print(f"Categories for game {game_name}: {game_details.get('categories', [])}")
              #   print(f"Genres for game {game_name}: {game_details.get('genres', [])}")
             
             if game_details is not None:
-                 vr_tag_present = any([category.get('description', '') == "VR Only" or category.get('description', '') == "VR Supported" for category in game_details.get('categories', [])]) or \
+                vr_tag_present = any([category.get('description', '') == "VR Only" or category.get('description', '') == "VR Supported" for category in game_details.get('categories', [])]) or \
                     any([genre.get('description', '') == 'VR Only' or genre.get('description', '') == 'VR Supported' for genre in game_details.get('genres', [])])
             
             if vr_tag_present:
                # print(f"VR tag found for game {game_name} with appid {appid}")
-                print(game_name)
-                all_games_data.append(game_data)  
+                print(f"{game_name} is vr")
+                all_games_data[appid] = game_data
+ 
                 if 'description' in game_details:
                     description = game_details['description']
                     try:
@@ -62,8 +94,9 @@ with open('new_games.json') as json_file:
                     except Exception as e:
                         print(f"Error during description summarization: {e}")
                         summarized_description = None
-                else:
-                    print(game_name)
+            else:
+                print(f"{game_name} is not vr")
+                pass
                         
         else:
             print(f"Request failed for appid:{appid}")
@@ -99,7 +132,7 @@ with open('old_games.json', 'r') as f:
     old_games = json.load(f)
 
 # Add new games to old games
-#old_games['applist']['apps'].extend(new_games['applist']['apps'])
+old_games['applist']['apps'].extend(new_games['applist']['apps'][:5])
 
 # Write the updated old games back to old_games.json
 with open('old_games.json', 'w') as f:
--- steam_store_poller.py
+++ steam_store_poller.py
@@ -5,6 +5,9 @@ def poll_steam():
     response = requests.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
     data = response.json()
 
+    open('new_games.json', 'w').close()
+
     with open('new_games.json', 'w') as f:
         json.dump(data, f)
         print("done polling.")
+
