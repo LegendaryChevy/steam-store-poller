8940d5509992c690e036de942cc9121d558521fc
legendarychevy <leeson73@gmail.com>
2024-05-07 12:45:31 -0700

    changed table names to match their new values and fixed iteration issue

--- db_loader.py
+++ db_loader.py
@@ -26,10 +26,6 @@ try:
 except Error as err:
     print(f"Error: {err}") 
 
-with open("game_data_output.json", "r") as f:
-    games = json.load(f)
-    for app_id, game_info in games.items():
-        game_data = game_info[app_id]["data"]
 
 
 def make_slug(name):            
@@ -54,13 +50,13 @@ def new_rating(input):
     
     return rating
 
-def single_player():
+def single_player(game_data):
     for category in game_data["categories"]:
         if category["description"] == "Single-Player":
             return 1
     return 0
     
-def multi_player():
+def multi_player(game_data):
     for category in game_data["categories"]:
         if category["description"] == "Multi-player":
             return 1
@@ -68,44 +64,47 @@ def multi_player():
 
    # Dictionary with column:value pairs
 
-
-for game in games:
-
-    data = {
-        "title": game_data["name"],
-        "slug": make_slug(game_data["name"]),
-        "description": game_data["ai_description"],
-        "short_description": game_data["short_description"],
-        "primary_image": f"pics/{game_data['name']}",
-        "age_rating": new_rating(game_data["ratings"]["dejus"]["rating"]),
-        "min_players": single_player(),
-        "max_players": multi_player(),
-        "store_url": game_data["url"],
-        "active": 1,
-        "steam_id": game_data["steam_appid"]
-    }
+with open("game_data_output.json", "r") as f:
+    games = json.load(f)
+    for app_id, game_info in games.items():
+        game_data = game_info[app_id]["data"]
 
 
-    steam_appid = game_data["steam_appid"]
+        data = {
+            "title": game_data["name"],
+            "slug": make_slug(game_data["name"]),
+            "description": game_data["ai_description"],
+            "short_description": game_data["short_description"],
+            "images": f"pics/{game_data['name']}",
+            "age_rating": new_rating(game_data["ratings"]["dejus"]["rating"]),
+            "single_player": single_player(game_data),
+            "multi_player": multi_player(game_data),
+            "store_url": game_data["url"],
+            "active": 1,
+            "steam_id": game_data["steam_appid"]
+        }
 
-        # SQL command to check if the steam_appid already exists in the database
-    sql_check = f"SELECT 1 FROM vr_titles WHERE steam_id = %s"
-    mysql_query.execute(sql_check, (steam_appid,))
 
-# If the steam_appid does not exist in the database then insert the data
-    if not mysql_query.fetchone():
-            # Dynamically building the SQL statement
-        columns = ', '.join(data.keys())
-        placeholders = ', '.join(['%s'] * len(data))
-        sql = f"INSERT INTO vr_titles ({columns}) VALUES ({placeholders})"
-            
-            # Executing the SQL command
-        mysql_query.execute(sql, list(data.values()))
-            
-            # Committing the changes to the database
-        mysql_client.commit()
-            
-        print("Data inserted successfully")
+        steam_appid = game_data["steam_appid"]
 
-    else:
-        pass
+            # SQL command to check if the steam_appid already exists in the database
+        sql_check = f"SELECT 1 FROM vr_titles WHERE steam_id = %s"
+        mysql_query.execute(sql_check, (steam_appid,))
+
+    # If the steam_appid does not exist in the database then insert the data
+        if not mysql_query.fetchone():
+                # Dynamically building the SQL statement
+            columns = ', '.join(data.keys())
+            placeholders = ', '.join(['%s'] * len(data))
+            sql = f"INSERT INTO vr_titles ({columns}) VALUES ({placeholders})"
+                
+                # Executing the SQL command
+            mysql_query.execute(sql, list(data.values()))
+                
+                # Committing the changes to the database
+            mysql_client.commit()
+                
+            print("Data inserted successfully")
+
+        else:
+            pass
