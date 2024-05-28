8cafee9d5786ec8db43e88b85fa986af7fe7ef03
cryptonaut <nrathman@ironcladtech.ca>
2024-05-28 11:07:23 -0700

    update poll_steam script

--- poll_steam.sh
+++ poll_steam.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Navigate to the directory containing the Python scripts
-cd /home/legendarychevy/projects/aagz/steam-store-poller/main || exit
-
 # Run game_filter.py
-python3 game_filter.py
+python3 main/game_filter.py
 
 # Check if the first script succeeded
 if [ $? -eq 0 ]; then
     # Run db_loader.py
-    python3 db_loader.py
+    python3 main/db_loader.py
 else
     echo "game_filter.py failed to run. db_loader.py will not be executed."
     exit 1
